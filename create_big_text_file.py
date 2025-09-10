b46f1f4851bd5b1cb3eeb1d7777748dad5dea47d
mihaibalica <balica@gmail.com>
2025-09-10 15:06:49 +0300

    big text files

---
+++ create_big_text_file.py
@@ -0,0 +1,25 @@
+import random
+import string
+import sys
+
+def create_big_text_file(filename, size, unit='GB'):
+    units = {'B': 1, 'KB': 1024, 'MB': 1024**2, 'GB': 1024**3}
+    size_bytes = int(size * units.get(unit.upper(), units['GB']))
+    chars = string.ascii_letters + string.digits + ".,<>/?;\'\\:\"|[]{}=+-_!@#$%^&*(), "
+    line_length = 75
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        written = 0
+        while written < size_bytes:
+            line = ''.join(random.choices(chars, k=line_length)) + '\n'
+            f.write(line)
+            written += len(line)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Create a large random text file.')
+    parser.add_argument('filename', help='Name of the file to create')
+    parser.add_argument('size', type=float, help='Total size of the file')
+    parser.add_argument('--unit', choices=['B', 'KB', 'MB', 'GB'], default='GB', help='Unit for file size (default: GB)')
+    args = parser.parse_args()
+    create_big_text_file(args.filename, args.size, args.unit)
